80c5c4a9478f056ef9884f0f54bda1611b17c177
kevin1688 <jackexi@gmail.com>
2022-01-14 10:20:54 +0800

    Initial Commit

---
+++ VGBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VGBox
+//
+//  Created by kai wen chen on 2022/1/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VGBox/VGBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VGBoxApp.swift
+//  VGBox
+//
+//  Created by kai wen chen on 2022/1/14.
+//
+
+import SwiftUI
+
+@main
+struct VGBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
